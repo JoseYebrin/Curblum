f665c619d2ca0dd031da520dbc55029794c5fc90
José Antonio Yebrin <joseantonioyebrin@hotmail.com>
2020-11-08 23:37:00 -0400

    Add files via upload

---
+++ effects.js
@@ -0,0 +1,276 @@
+function isTouchDevice() {
+  let elm = document.createElement('div');
+  elm.setAttribute('ongesturestart', 'return;'); // or try "ontouchstart"
+  return typeof elm.ongesturestart === "function";
+}
+
+/*Button Hover Effects START*/
+const ANIMATEDCLASSNAME = "animated";
+const ELEMENTS = document.querySelectorAll(".HOVER");
+const ELEMENTS_SPAN = [];
+
+ELEMENTS.forEach((element, index) => {
+	let addAnimation = false;
+	// Elements that contain the "FLASH" class, add a listener to remove
+	// animation-class when the animation ends
+	if (element.classList[1] == "FLASH") {
+		element.addEventListener("animationend", e => {
+			element.classList.remove(ANIMATEDCLASSNAME);
+		});
+		addAnimation = true;
+	}
+
+	// If The span element for this element does not exist in the array, add it.
+	if (!ELEMENTS_SPAN[index])
+		ELEMENTS_SPAN[index] = element.querySelector("span");
+
+	element.addEventListener("mouseover", e => {
+		ELEMENTS_SPAN[index].style.left = e.pageX - element.offsetLeft + "px";
+		ELEMENTS_SPAN[index].style.top = e.pageY - element.offsetTop + "px";
+
+		// Add an animation-class to animate via CSS.
+		if (addAnimation) element.classList.add(ANIMATEDCLASSNAME);
+	});
+
+	element.addEventListener("mouseout", e => {
+		ELEMENTS_SPAN[index].style.left = e.pageX - element.offsetLeft + "px";
+		ELEMENTS_SPAN[index].style.top = e.pageY - element.offsetTop + "px";
+	});
+});
+/*Button Hover Effects END*/
+/*Mobile Menu Icon START*/
+var icon_1 = document.getElementById("i1");
+var mobileNav = document.getElementById("mobileNav");
+var topLine_1 = document.getElementById("top-line-1");
+var middleLine_1 = document.getElementById("middle-line-1");
+var bottomLine_1 = document.getElementById("bottom-line-1");
+var state_1 = "menu";  // can be "menu" or "arrow"
+var topLineY_1;
+var middleLineY_1;
+var bottomLineY_1;
+var topLeftY_1;
+var topRightY_1;
+var bottomLeftY_1;
+var bottomRightY_1;
+var topLeftX_1;
+var topRightX_1;
+var bottomLeftX_1;
+var bottomRightX_1;
+
+///Animation Variables
+var segmentDuration_1 = 15;
+var menuDisappearDurationInFrames_1 = segmentDuration_1;
+var arrowAppearDurationInFrames_1 = segmentDuration_1;
+var arrowDisappearDurationInFrames_1 = segmentDuration_1;
+var menuAppearDurationInFrames_1 = segmentDuration_1;
+var menuDisappearComplete_1 = false;
+var arrowAppearComplete_1 = false;
+var arrowDisappearComplete_1 = false;
+var menuAppearComplete_1 = false;
+var currentFrame_1 = 1;
+
+///Menu Disappear 
+function menuDisappearAnimation_1() {
+	currentFrame_1++;
+	if ( currentFrame_1 <= menuDisappearDurationInFrames_1 ) {
+		window.requestAnimationFrame( ()=> { 
+			//top line
+			topLineY_1 = AJS.easeInBack( 37, 50, menuDisappearDurationInFrames_1, currentFrame_1 );
+			topLine_1.setAttribute( "d", "M30,"+topLineY_1+" L70,"+topLineY_1 );
+			//bottom line
+			bottomLineY_1 = AJS.easeInBack( 63, 50, menuDisappearDurationInFrames_1, currentFrame_1 );
+			bottomLine_1.setAttribute( "d", "M30,"+bottomLineY_1+" L70,"+bottomLineY_1 );
+			//recursion
+			menuDisappearAnimation_1();
+		});
+	} else {
+		middleLine_1.style.opacity = "0";
+		currentFrame_1 = 1;
+		menuDisappearComplete_1 = true;
+		openMenuAnimation_1();
+	}
+}
+
+///Cross Appear
+function arrowAppearAnimation_1() {
+	currentFrame_1++;
+	if ( currentFrame_1 <= arrowAppearDurationInFrames_1 ) {
+		window.requestAnimationFrame( ()=> { 
+			//top line
+			topLeftX_1 = AJS.easeOutBack( 30, 35, arrowAppearDurationInFrames_1, currentFrame_1 );
+			topLeftY_1 = AJS.easeOutBack( 50, 35, arrowAppearDurationInFrames_1, currentFrame_1 );
+			bottomRightX_1 = AJS.easeOutBack( 70, 65, arrowAppearDurationInFrames_1, currentFrame_1 );
+			bottomRightY_1 = AJS.easeOutBack( 50, 65, arrowAppearDurationInFrames_1, currentFrame_1 );
+			topLine_1.setAttribute( "d", "M" + topLeftX_1 + "," + topLeftY_1 + " L" + bottomRightX_1 + "," + bottomRightY_1 );
+			//bottom line
+			bottomLeftX_1 = AJS.easeOutBack( 30, 35, arrowAppearDurationInFrames_1, currentFrame_1 );
+			bottomLeftY_1 = AJS.easeOutBack( 50, 65, arrowAppearDurationInFrames_1, currentFrame_1 );
+			topRightX_1 = AJS.easeOutBack( 70, 65, arrowAppearDurationInFrames_1, currentFrame_1 );
+			topRightY_1 = AJS.easeOutBack( 50, 35, arrowAppearDurationInFrames_1, currentFrame_1 );
+			bottomLine_1.setAttribute( "d", "M" + bottomLeftX_1 + "," + bottomLeftY_1 + " L" + topRightX_1 + "," + topRightY_1 );
+			//recursion
+			arrowAppearAnimation_1();
+		});
+	} else {
+		currentFrame_1 = 1;
+		arrowAppearComplete_1 = true;
+		openMenuAnimation_1();
+	}
+}
+
+///Combined Open Menu Animation
+function openMenuAnimation_1() {
+	if ( !menuDisappearComplete_1 ) { 
+		menuDisappearAnimation_1();
+	} else if ( !arrowAppearComplete_1) {
+		arrowAppearAnimation_1();
+	}
+}
+
+///Cross Disappear
+function arrowDisappearAnimation_1() {
+	currentFrame_1++;
+	if ( currentFrame_1 <= arrowDisappearDurationInFrames_1 ) {
+		window.requestAnimationFrame( ()=> {
+			//top line
+			topLeftX_1 = AJS.easeInBack( 35, 30, arrowDisappearDurationInFrames_1, currentFrame_1 );
+			topLeftY_1 = AJS.easeInBack( 35, 50, arrowDisappearDurationInFrames_1, currentFrame_1 );
+			bottomRightX_1 = AJS.easeInBack( 65, 70, arrowDisappearDurationInFrames_1, currentFrame_1 );
+			bottomRightY_1 = AJS.easeInBack( 65, 50, arrowDisappearDurationInFrames_1, currentFrame_1 );
+			topLine_1.setAttribute( "d", "M" + topLeftX_1 + "," + topLeftY_1 + " L" + bottomRightX_1 + "," + bottomRightY_1 );
+			//bottom line
+			bottomLeftX_1 = AJS.easeInBack( 35, 30, arrowDisappearDurationInFrames_1, currentFrame_1 );
+			bottomLeftY_1 = AJS.easeInBack( 65, 50, arrowDisappearDurationInFrames_1, currentFrame_1 );
+			topRightX_1 = AJS.easeInBack( 65, 70, arrowDisappearDurationInFrames_1, currentFrame_1 );
+			topRightY_1 = AJS.easeInBack( 35, 50, arrowDisappearDurationInFrames_1, currentFrame_1 );
+			bottomLine_1.setAttribute( "d", "M" + bottomLeftX_1 + "," + bottomLeftY_1 + " L" + topRightX_1 + "," + topRightY_1 );
+			//recursion
+			arrowDisappearAnimation_1();
+		});
+	} else {
+		middleLine_1.style.opacity = "1";
+		currentFrame_1 = 1;
+		arrowDisappearComplete_1 = true;
+		closeMenuAnimation_1();
+	}
+}
+
+///Menu Appear
+function menuAppearAnimation_1() {
+	currentFrame_1++;
+	if ( currentFrame_1 <= menuAppearDurationInFrames_1 ) {
+		window.requestAnimationFrame( ()=> {
+			//top line
+			topLineY_1 = AJS.easeOutBack( 50, 37, menuDisappearDurationInFrames_1, currentFrame_1 );
+			topLine_1.setAttribute( "d", "M30,"+topLineY_1+" L70,"+topLineY_1 );
+			//bottom line
+			bottomLineY_1 = AJS.easeOutBack( 50, 63, menuDisappearDurationInFrames_1, currentFrame_1 );
+			bottomLine_1.setAttribute( "d", "M30,"+bottomLineY_1+" L70,"+bottomLineY_1 );
+			//recursion
+			menuAppearAnimation_1();
+		});
+	} else {
+		currentFrame_1 = 1;
+		menuAppearComplete_1 = true;
+		closeMenuAnimation_1();
+	}
+}
+
+///Close Menu Animation
+function closeMenuAnimation_1() {
+	if ( !arrowDisappearComplete_1 ) {
+		arrowDisappearAnimation_1();
+	} else if ( !menuAppearComplete_1 ) {
+		menuAppearAnimation_1();
+	}
+}
+
+///Main Function
+function main() {
+	if ( state_1 === "menu" ) {
+  	openMenuAnimation_1();
+  	state_1 = "arrow";
+  	arrowDisappearComplete_1 = false;
+		menuAppearComplete_1 = false;
+  } else if ( state_1 === "arrow" ) {
+  	closeMenuAnimation_1();
+  	state_1 = "menu";
+  	menuDisappearComplete_1 = false;
+		arrowAppearComplete_1 = false;
+  }
+}
+
+///Events
+icon_1.addEventListener( "click", ()=> {
+	if (!icon_1.classList.contains("disabled")) {
+		main()
+	}
+});
+mobileNav.addEventListener( "click", (e)=> {
+	if (icon_1.classList.contains("disabled") == false) {
+		if (e.target == document.getElementById("mobileNav")) {
+			main();
+		}
+	}
+});
+/*Mobile Menu Icon END*/
+/*Image Tilt Effect START*/
+/* Store the element in el */
+let el = document.getElementById('tilt')
+
+/* Get the height and width of the element */
+const height = el.clientHeight
+const width = el.clientWidth
+
+/*
+  * Add a listener for mousemove event
+  * Which will trigger function 'handleMove'
+  * On mousemove
+  */
+el.addEventListener('mousemove', handleMove)
+
+/* Define function a */
+function handleMove(e) {
+  /*
+    * Get position of mouse cursor
+    * With respect to the element
+    * On mouseover
+    */
+  /* Store the x position */
+  const xVal = e.layerX
+  /* Store the y position */
+  const yVal = e.layerY
+  
+  /*
+    * Calculate rotation valuee along the Y-axis
+    * Here the multiplier 20 is to
+    * Control the rotation
+    * You can change the value and see the results
+    */
+  const yRotation = 20 * ((xVal - width / 2) / width)
+  
+  /* Calculate the rotation along the X-axis */
+  const xRotation = -20 * ((yVal - height / 2) / height)
+  
+  /* Generate string for CSS transform property */
+  const string = 'perspective(500px) scale(1.1) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)'
+  
+  /* Apply the calculated transformation */
+  el.style.transform = string
+}
+
+/* Add listener for mouseout event, remove the rotation */
+el.addEventListener('mouseout', function() {
+  el.style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)'
+})
+
+/* Add listener for mousedown event, to simulate click */
+el.addEventListener('mousedown', function() {
+  el.style.transform = 'perspective(500px) scale(0.9) rotateX(0) rotateY(0)'
+})
+
+/* Add listener for mouseup, simulate release of mouse click */
+el.addEventListener('mouseup', function() {
+  el.style.transform = 'perspective(500px) scale(1.1) rotateX(0) rotateY(0)'
+})
+/*Image Tilt Effect END*/
